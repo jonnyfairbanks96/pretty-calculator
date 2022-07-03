f4fd93004f69ec3b57bca11b985533b3ecf9038b
Jonathan Fairbanks <fairbanks.jonathan96@gmail.com>
2022-07-03 16:41:07 -0600

    Working calculator

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,30 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn compute(previous_op: String, current_op: String, operator: String) -> Result<f64, &'static str> {
+    let current: f64 = match current_op.parse() {
+        Ok(num) => num,
+        Err(_) => return Err("Opps looks like you forgot something, try again!"),
+    };
+    let previous: f64 = match previous_op.parse() {
+        Ok(num) => num,
+        Err(_) => return Err("Opps looks like you forgot something, try again!"),
+    };
+    return match operator.as_str() {
+        "+" => Ok(previous + current),
+        "-" => Ok(previous - current),
+        "/" => Ok(previous / current),
+        "*" => Ok(previous * current),
+        _ => Err("Opps cannot compute that one, try again!"),
+    };
+}
+
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .menu(if cfg!(target_os = "macos") {
-      tauri::Menu::os_default(&context.package_info().name)
-    } else {
-      tauri::Menu::default()
-    })
-    .run(context)
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![compute])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/components/PrettyCalculator.vue
+++ src/components/PrettyCalculator.vue
@@ -1,5 +1,6 @@
 <script>
 import Button from './Button.vue'
+import { invoke } from '@tauri-apps/api/tauri'
 
 export default {
   components: {
@@ -7,19 +8,75 @@ export default {
   },
   data() {
     return {
-      total: "",
-      calculation: "",
+      currentOp: "",
+      previousOp: "",
+      operator: "",
+      error: "",
     }
   },
   methods: {
+    alertErr(err) {
+      this.error = err;
+      setTimeout(() => {this.error = ""}, 5000);
+    },
+    del() {
+      this.currentOp = this.currentOp.slice(0, -1)
+    },
     reset() {
-      this.calculation = "";
+      this.currentOp = "";
+      this.previousOp = "";
+      this.operator = "";
+      this.total = "";
     },
     addNumber(numberToAdd) {
-      this.calculation += numberToAdd;
+      if (this.previousOp != "" && this.operator == "") {
+        this.reset();
+        this.currentOp += numberToAdd;
+      } else {
+        this.currentOp += numberToAdd;
+      }
     },
-    addOpp(op) {
-      this.calculation += op + "\n";
+    addOp(op) {
+      if (this.previousOp != "" && this.currentOp != "") {
+        this.computeTotal(() => this.operator = op);
+      } else {
+        this.previousOp += this.currentOp;
+        this.currentOp = "";
+        this.operator = "";
+        this.operator = op;
+      }
+    },
+    computeTotal(fun = () => {}) {
+      if (this.currentOp != "" && this.previousOp != "" && this.operator != "") {
+        invoke(
+          'compute', {
+            previousOp: this.previousOp,
+            currentOp: this.currentOp,
+            operator: this.operator
+          }
+        ).then((result) => {
+          this.reset();
+          this.previousOp = String(result);
+        }).then(fun)
+        .catch((err) => {
+          this.alertErr(err)
+          this.reset();
+        })
+      } else {
+        this.alertErr("Ooops try again!");
+      }
+    },
+    percent() {
+      if (this.previousOp != "") {
+        this.operator = "/";
+        this.currentOp = "100";
+        this.computeTotal();
+      } else {
+        this.previousOp = this.currentOp;
+        this.operator = "/";
+        this.currentOp = "100";
+        this.computeTotal();
+      }
     }
   },
 }
@@ -28,40 +85,44 @@ export default {
 <template>
   <div class="container mt-10 p-4 m-auto">
     <div class="grid grid-cols-4 gap-4 h-48">
-      <h1 class="col-span-4 text-center text-xl p-5 text-cyan-200">The Pretty
-        Calculator</h1>
-      <!-- first row  -->
+      <h1 class="col-span-4 text-center text-xl p-5 text-cyan-200">
+        The Pretty Calculator
+      </h1>
+      <span class="text-xl col-span-4 text-center text-red-200">
+        {{ error }}
+      </span>
       <div
-        class="p-5 bg-gradient-to-l from-cyan-400 to-blue-200 rounded-lg col-span-4">
-        <span class="text-left text-black">
-          {{ calculation }}
+          class="grid col-span-4 m-0 p-5 bg-gradient-to-r from-cyan-400 to-blue-200 rounded-lg col-span-4">
+        <span class="text-left text-grey-300">
+          {{ previousOp }}
+          {{ operator }}
         </span>
-        <span class="text-right text-black">
-          {{ total }}
+        <span class="text-xl text-left text-black">
+          {{ currentOp }}
         </span>
       </div>
-      <!-- second row  -->
       <Button @click="reset" text="AC"/>
-      <Button text="&#177"/>
-      <Button text="&#37;"/>
-      <Button @click="addOpp('/')" text="&#247;"/>
+      <Button @click="del" text="&#8592;" />
+      <Button @click="percent" text="&#37;"/>
+      <Button @click="addOp('/')" text="&#247;"/>
       <Button @click="addNumber(7)" text="7"/>
       <Button @click="addNumber(8)" text="8"/>
       <Button @click="addNumber(9)" text="9"/>
-      <Button text="&#215;"/>
+      <Button @click="addOp('*')" text="&#215;"/>
+
       <Button @click="addNumber(4)" text="4"/>
       <Button @click="addNumber(5)" text="5"/>
       <Button @click="addNumber(6)" text="6"/>
-      <Button text="&#45;"/>
+      <Button @click="addOp('-')" text="&#45;"/>
 
       <Button @click="addNumber(1)" text="1"/>
       <Button @click="addNumber(2)" text="2"/>
       <Button @click="addNumber(3)" text="3"/>
-      <Button text="&#43;"/>
-      <!-- sixth row  -->
-      <Button @click="addNumber(0)" class="col-span-2" text="0"/>
-      <Button text="."/>
-      <Button text="&#61;"/>
+      <Button @click="addOp('+')" text="&#43;"/>
+
+      <Button @click="addNumber(0)" text="0"/>
+      <Button @click="addNumber('.')" text="."/>
+      <Button @click="computeTotal" class="col-span-2" text="&#61;"/>
     </div>
   </div>
 </template>
